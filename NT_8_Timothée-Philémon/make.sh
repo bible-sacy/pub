1549c6dedb12cd6edf9c96325ce953fe480ca522
Louis-Guillaume Dubois <contact@lgdubois.fr>
2022-08-31 14:37:08 +0200

    NT_8 et adresse de contact

---
+++ NT_8_Timothée-Philémon/make.sh
@@ -0,0 +1,9 @@
+PAGES=$(echo ../../pngs-nt-2/timothee_tite/timothee_tite-{6..7}.png ../../pngs-nt-2/timothee_tite/timothee_tite-{10..410}.png)
+exiftool -overwrite_original -all= $PAGES
+soffice --convert-to png info.txt
+img2pdf $PAGES info.png -o pngs.pdf
+python -m pagelabels --delete pngs.pdf
+python -m pagelabels --startpage 1 --type 'roman lowercase' pngs.pdf
+python -m pagelabels --startpage 17 --firstpagenum 1 pngs.pdf
+../pdfoutline.py pngs.pdf pdf.toc NT_8_Timothée-Philémon.pdf
+exiftool -overwrite_original -Title="[NT 8] Épîtres de St Paul à Timothée, Tite & Philémon" -Author="Le Maistre de Sacy" -Subject="$(tr '\n' ' ' <info.txt)" -Creator='https://bible.sacy.be/pub/' NT_8_Timothée-Philémon.pdf
