7c0ca9809946934304dc51837d1546b2aaab4fea
Louis-Guillaume Dubois <contact@lgdubois.fr>
2022-09-03 15:44:01 +0200

    AT_14 (en cours)

---
+++ AT_14_Psaumes_I-LIV/make.sh
@@ -0,0 +1,9 @@
+PAGES=$(echo ../../pngs-at/1711_desprez_pseaumes_t1/1711_desprez_pseaumes_t1-{6..772}.png)
+exiftool -overwrite_original -all= $PAGES
+soffice --convert-to png info.txt
+img2pdf $PAGES info.png -o pngs.pdf
+python -m pagelabels --delete pngs.pdf
+python -m pagelabels --startpage 1 --type 'roman lowercase' pngs.pdf
+python -m pagelabels --startpage 49 --firstpagenum 1 pngs.pdf
+../pdfoutline.py pngs.pdf pdf.toc AT_14_Psaumes_I-LIV.pdf
+exiftool -overwrite_original -Producer='https://bible.sacy.be/pub/' -Author="Le Maistre de Sacy" -Creator="Le Maistre de Sacy" -Title="[AT 14] Psaumes I-LIV" AT_14_Psaumes_I-LIV.pdf
